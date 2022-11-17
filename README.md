3b560e2567d94e9a0153fe03cf8f9634f86e3131
garemar <gabrielamartinez0908@hotmail.com>
2022-11-16 23:11:23 -0300

    firstAproach

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # gitHubUsers
+
+### Funcionalidad.
+Servidor creado con express-generator que consume api de gitHub con node-fetch para poder buscar usuarios.
+
+### Comando para su funcionamiento.
+- npm install
+- npm start
+
+> Ingresar a a su plataforma de API's como Postman o desde el navegador y realizar la consulta (GET) http://localhost:3000/api/users/{nombreDeUsuario}. 
+>>  Ejemplo http://localhost:3000/api/users/garemar
---
+++ src/controllers/usersControllers.js
@@ -0,0 +1,18 @@
+const path = require('path');
+const fetch = require('node-fetch');
+
+
+
+
+const usersController = {
+  findUser: async function (req, res) {
+    const user = req.params.user
+    fetch('https://api.github.com/users/' + user).then(res => res.json()).then(json => {
+      return res.status(200).json({
+        json
+      })
+    }).catch(e => { return res.status(404).json })
+
+  }
+}
+module.exports = usersController;
